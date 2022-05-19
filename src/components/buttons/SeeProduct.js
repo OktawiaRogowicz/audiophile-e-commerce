2a5513657a866dc6d56d30838260ce2cfb6d5272
OktawiaRogowicz <oktawia.rogowicz.30@gmail.com>
2022-05-19 21:50:26 +0200

    Fixed buttons

--- src/components/buttons/SeeProduct.js
+++ src/components/buttons/SeeProduct.js
@@ -3,21 +3,45 @@ import styled from "styled-components";
 const Container = styled.button`
     height: 3rem;
     width: 10rem;
-    background-color: var(--orange);
     text-transform: uppercase;
-    color: white;
     border: none;
+
+    &:hover, &:active, &:focus {
+        color: var(--white);
+    }
+`
+
+const Version1 = styled(Container)`
+    background-color: var(--orange);
+    color: white;
 
     &:hover, &:active, &:focus {
         background-color: var(--light_orange);
     }
 `
 
-function SeeProductButton() {
+const Version2 = styled(Container)`
+    background-color: var(--white);
+    color: var(--black);
+
+    border: 0.094rem solid var(--black);
+
+    &:hover, &:active, &:focus {
+        background-color: var(--black);
+    }
+`
+
+function SeeProductButton({version}) {
+
+    const ButtonText = "See Product";
+
     return(
-        <Container>
-            See Product
-        </Container>
+        <div>
+            { version === "orange" ?
+                <Version1>{ButtonText}</Version1> : 
+                <Version2>{ButtonText}</Version2>
+            }
+        </div>
     );
 }
 
--- src/components/buttons/ShopButton.js
+++ src/components/buttons/ShopButton.js
@@ -1,13 +1,17 @@
 import styled from "styled-components";
+import arrow from "../../audiophile-ecommerce-website/starter-code/assets/shared/desktop/icon-arrow-right.svg"
 
-const Container = styled.button`
-
+const Container = styled.a`
+    img {
+        margin: 0 0 0 0.3rem;
+    }
 `
 
 function ShopButton() {
     return(
         <Container>
             Shop
+            <img src={arrow}></img> 
         </Container>
     );
 }
