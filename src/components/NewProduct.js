858a41674ad884399d00c366ba683f08ff36ce64
OktawiaRogowicz <oktawia.rogowicz.30@gmail.com>
2022-05-19 21:49:15 +0200

    Fixed new product background due to background colour size changes in app

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -1,20 +1,37 @@
 import styled from "styled-components";
 import SeeProductButton from "./buttons/SeeProduct";
+import image from "../audiophile-ecommerce-website/starter-code/assets/home/desktop/image-hero.jpg";
 
 const Container = styled.div`
+    height: 39.5rem;
     display: flex;  
     flex-direction: row;
 
-    background-color: black;
+    box-sizing: border-box;
+    padding: 0 9rem 0 9rem;
+
+    background-color: transparent;
 `
 
 const HalfContainer = styled.div`
     width: 100%;
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
 
     color: white;
+
+    h6 {
+        opacity: 0.5;
+        margin: 0;
+    }
+
+    p {
+        color: var(--grey);
+        max-width: 23rem;
+        margin: 2rem 0 3rem 0;
+    }
 `
 
 function NewProduct() {
@@ -22,14 +39,14 @@ function NewProduct() {
         <Container>
             <HalfContainer>
                 <div>
-                    <h6>New product</h6>
+                    <h6 id="overline">New product</h6>
                     <h1>XX99 Mark II Headphones</h1>
                     <p>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
-                    <SeeProductButton></SeeProductButton>
+                    <SeeProductButton version="orange"></SeeProductButton>
                 </div>
             </HalfContainer>
             <HalfContainer>
-                <img></img>
+                {/* <img src={image}></img> */}
             </HalfContainer>
         </Container>
     );
