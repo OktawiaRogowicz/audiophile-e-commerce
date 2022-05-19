4a56177205939d27c1907906130563cd1c2ccdd1
OktawiaRogowicz <oktawia.rogowicz.30@gmail.com>
2022-05-19 21:49:46 +0200

    Finished product categories

--- src/components/ProductCategories.js
+++ src/components/ProductCategories.js
@@ -1,16 +1,24 @@
 import styled from "styled-components";
 import ProductCategory from "./ProductCategory";
 
-const Container = styled.div`
+import headphonesImage from "../audiophile-ecommerce-website/starter-code/assets/shared/desktop/image-category-thumbnail-headphones.png";
+import speakersImage from "../audiophile-ecommerce-website/starter-code/assets/shared/desktop/image-category-thumbnail-speakers.png";
+import earphonesImage from "../audiophile-ecommerce-website/starter-code/assets/shared/desktop/image-category-thumbnail-earphones.png";
 
+const Container = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
 `
 
 function ProductCategories() {
     return(
         <Container>
-            <ProductCategory categoryName="Headphones"/>
-            <ProductCategory categoryName="Speakers"/>
-            <ProductCategory categoryName="Earphones"/>
+            <ProductCategory categoryName="Headphones" categoryImage={headphonesImage}/>
+            <ProductCategory categoryName="Speakers" categoryImage={speakersImage}/>
+            <ProductCategory categoryName="Earphones" categoryImage={earphonesImage}/>
         </Container>
     );
 }
--- src/components/ProductCategory.js
+++ src/components/ProductCategory.js
@@ -2,14 +2,49 @@ import styled from "styled-components";
 import ShopButton from "./buttons/ShopButton";
 
 const Container = styled.div`
+    height: 17.813rem;
+    width: 100%;
+    max-width: 350px;
+    position: relative;
 
+    box-sizing: border-box;
+    padding: 1.5rem;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: center;
+
+    z-index: 1;
+
+    img {
+        max-width: 75%;
+        margin: 0;
+    }
+    h6 {
+        margin: 0.25rem;
+    }
+`
+
+const Background = styled.div`
+    position: absolute;
+    min-width: 100%;
+    height: 75%;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    border-radius: 0.313rem;
+    background-color: var(--light_grey);
+    z-index: -1;
 `
 
 function ProductCategory({categoryName, categoryImage}) {
     return(
         <Container>
-            <h5>{categoryName}</h5>
-            <ShopButton></ShopButton>
+                <Background/>
+                <img src={categoryImage}></img>
+                <h6>{categoryName}</h6>
+                <ShopButton></ShopButton>
         </Container>
     );
 }
