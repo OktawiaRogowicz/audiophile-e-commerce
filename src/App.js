f59b0c43fe59dfdee3784230181daeff7f97551f
OktawiaRogowicz <oktawia.rogowicz.30@gmail.com>
2022-06-02 22:19:52 +0200

    Added scroll to top component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Routes, Route, Navigate, } from 'react-router-dom';
 import HomePage from './components/HomePage';
